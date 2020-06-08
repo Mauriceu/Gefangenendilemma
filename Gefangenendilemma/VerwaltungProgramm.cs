39f87e442a6df3f732ba401ab471512acfbdacec
Neuhier <hikmetozer@rocketmail.com>
2020-06-08 10:22:22 +0200

    Rückgängig machen des Fehlerhaften Punkteresets

--- Gefangenendilemma/VerwaltungProgramm.cs
+++ Gefangenendilemma/VerwaltungProgramm.cs
@@ -12,8 +12,7 @@ namespace Gefangenendilemma
         /// </summary>
         private static List<BasisStrategie> _strategien;
 
-        private static int gesamtPunkte1, gesamtPunkte2, schwere, runde;
-        private static int punkte1, punkte2 = 0;
+        private static int punkte1, punkte2, gesamtPunkte1, gesamtPunkte2, schwere, runde;
         private static bool automatischesVerhoer;
 
         static void Main(string[] args)
